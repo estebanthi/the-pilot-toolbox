f955636c40b4d3e401b9a3a769a3abb3010e0302
estebanthi <77675611+estebanthi@users.noreply.github.com>
2022-04-19 20:52:09 +0200

    Footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,8 +1,45 @@
+import Link from "next/link";
+
+import styles from "./Footer.module.css"
+
+
 const Footer = () => {
 
     return (
-        <div>
-
+        <div className={styles.footer}>
+            <hr className={styles.line}/>
+            <div className={styles.container}>
+                <div className={styles.subContainer}>
+                    <h3 className={styles.title}>Navigation</h3>
+                    <Link href={"/"}>
+                        <span>Accueil</span>
+                    </Link>
+                    <Link href={"/qcm"}>
+                        <span>QCM</span>
+                    </Link>
+                    <Link href={"/subscription"}>
+                        <span>Abonnement</span>
+                    </Link>
+                    <Link href={"/login"}>
+                        <span>Se connecter</span>
+                    </Link>
+                    <Link href={"/register"}>
+                        <span>Créer un compte</span>
+                    </Link>
+                </div>
+                <div className={styles.subContainer}>
+                    <h3 className={styles.title}>Pages légales</h3>
+                    <Link href={"/politique-de-confidentialité"}>
+                        <span>Politique de confidentialité</span>
+                    </Link>
+                    <Link href={"/mentions-legales"}>
+                        <span>Mentions légales</span>
+                    </Link>
+                    <Link href={"/cgv"}>
+                        <span>Conditions générales de vente</span>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 
