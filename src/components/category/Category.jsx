a4e7fb3217a14d119cec1d01890f276892e5dbaa
estebanthi <77675611+estebanthi@users.noreply.github.com>
2022-04-28 10:00:20 +0200

    test

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -22,7 +22,7 @@ const Category = (props) => {
 
     return (
         <Grid item xs={props.xs ? props.xs : 4} className={styles.container} onClick={() => router.push("/qcm/"+props.category.slug)}>
-            {props.image}
+            <div className={styles.image}>{props.image}</div>
             <h3>{props.category.full_name}</h3>
             <span>{qcmNb} QCM</span>
         </Grid>
